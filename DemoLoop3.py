be4d5775699a627e2a2e48585c9acb1f95e4edde
rmswl82 <rmswlwlswn@naver.com>
2021-11-30 13:30:16 +0900

    break continue

---
+++ DemoLoop3.py
@@ -0,0 +1,16 @@
+# DemoLoop3.py
+
+lst = [1,2,3,4,5,6,7,8,9,10]
+
+for i in lst:
+    if i > 5:
+        break
+    print("item:{0}".format(i))
+
+print("---continue---")
+for i in lst:
+    if i % 2 == 0:
+        continue
+    print("item:{0}".format(i))
+
+    
--- demoLoof2.py
+++ demoLoof2.py
@@ -4,4 +4,5 @@ lst = [1,2,3]
 
 for i in lst:
     print(i)
-    
+
+
