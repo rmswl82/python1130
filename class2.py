109166b09feffb5843eaa477fb0042c51ffff6b2
rmswl82 <rmswlwlswn@naver.com>
2021-12-01 11:33:39 +0900

    class 연습

---
+++ class2.py
@@ -0,0 +1,24 @@
+# class2.py
+#1) 클래스 형식을 정의
+
+class Person:
+    #클래스에 소속된 데이터 공유하는 멤버 변수
+    num_person = 0
+
+    #생성자(초기화)메소드
+    def __init__(self):
+        #인스턴스의 멤버 변수를 초기화
+        self.name = "default name"
+        #클래스 자체에 소속된 의미
+        Person.num_person += 1
+    def print(self):
+        print("My name is {0}".format(self.name))
+
+#2)인스턴스 생성
+p1 = Person()
+p2 = Person()
+print("인스턴트 갯수:{0}".format(Person.num_person))
+
+
+
+
