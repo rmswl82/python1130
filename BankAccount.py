ef9472e74de8c24d8846ed267f5990b2c034774b
rmswl82 <rmswlwlswn@naver.com>
2021-12-01 14:25:51 +0900

    BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -2,20 +2,25 @@
 
 #은행의 계정을 표현한 클래스 
 class BankAccount:
+    #초기화 메서드
     def __init__(self, id, name, balance):
         self.id = id
         self.name = name 
         self.balance = balance 
+    #입금 메서드
     def deposit(self, amount):
         self.balance += amount 
+    #출금 메서도
     def withdraw(self, amount):
         self.balance -= amount
+    #문자열을 출력하는 메서드(ToStrint())
     def __str__(self):
         return "{0}, {1}, {2}".format(self.id, 
         self.name, self.balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
+account1.deposit(5000)
 account1.withdraw(3000)
 print(account1)
 
--- 메모리관리.py
+++ 메모리관리.py
@@ -1,12 +1,17 @@
 # 메모리관리.py 
 
 class MyClass:
+    #생성자(초기화를 담당)
     def __init__(self, value):
         self.value = value 
         print("Instance is created! value=", value)
+    #소멸자 메서드(마지막에 정리 작업)
     def __del__(self):
         print("Instance is deleted!")
 
+#인스턴스 생성
 d = MyClass(5)
 del d 
+
+print("코드 실행 종료")
 
