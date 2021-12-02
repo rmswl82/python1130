a86f634353987a22c79fac4ddcaee1efe1f7b583
rmswl82 <rmswlwlswn@naver.com>
2021-12-02 13:19:14 +0900

    문자열 처리연습

--- DemoStr.py
+++ DemoStr.py
@@ -5,4 +5,10 @@ for name in names:
     print("안녕하세요 {0}님 추운 겨울입니다.".format(name))
     print("="*40)
 
-    
+strA= "python is very powerful"
+print(len(strA))
+print(strA.capitalize())
+print(strA.count("p"))
+print(strA.count("p", 7))
+
+
