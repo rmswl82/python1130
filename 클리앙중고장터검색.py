fc33882fd004f244541ce7fe9a8703eb88c8b4d9
rmswl82 <rmswlwlswn@naver.com>
2021-12-02 16:31:43 +0900

    중고장터 크롤링2

--- 클리앙중고장터검색.py
+++ 클리앙중고장터검색.py
@@ -4,16 +4,18 @@ import urllib.request
 import re 
 
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
+#hdr은 header의 약자
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
 for n in range(0,10):
         #클리앙의 중고장터 주소 
         data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+        print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
                                     headers = hdr)
-        req = urllib.request.Request(data)
         data = urllib.request.urlopen(req).read()
+        #한글이 깨지는 경우는 디코딩
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
 
@@ -26,13 +28,10 @@ for n in range(0,10):
 
         for item in list:
                 try:
-                        #<a class='list_subject'><span>text</span><span>text</span>
-                        span = item.contents[1]
-                        span2 = span.nextSibling.nextSibling
-                        title = span2.text 
+                        title = item.text
+
                         if (re.search('아이폰', title)):
                                 print(title.strip())
-                                print('https://www.clien.net'  + item['href'])
                 except:
                         pass
         
